3728e77fc1414739528dbe40ba43507a0ffe66a0
Javier Otegui <javier.otegui@gmail.com>
2012-09-26 19:27:59 -0600

    App virtual folder created

--- biddsat.py
+++ biddsat.py
@@ -1,10 +1,14 @@
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-      html_source=open('/home/jotegui/code/BIDDSAT/index.html','r').read()
-      self.response.headers['Content-Type'] = 'text/HTML'
-      self.response.write(html_source)
+	def get(self):
+		html_source=open('/home/jotegui/code/BIDDSAT/index.html','r').read()
+		self.response.headers['Content-Type'] = 'text/HTML'
+		self.response.write(html_source)
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+class Biddsat(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['Content-type'] = 'text/HTML'
+		self.response.write('Hey there, it\'s me, BIDDSAT')
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/app/',Biddsat)], debug=True)
